882e5507cad423eee2deed6931e34507a6db28f5
SDETaditya <aditya.automationqa@gmail.com>
2026-08-09 04:09:35 +0530

    complete standalone test with correct sauce labs website for purchasing item

--- sauce_labs_selenium/src/test/java/com/aditya/StandaloneTest.java
+++ sauce_labs_selenium/src/test/java/com/aditya/StandaloneTest.java
@@ -3,12 +3,16 @@ package com.aditya;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -16,76 +20,54 @@ import org.testng.Assert;
 public class StandaloneTest {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        String productName = "Grey jacket";
-        WebDriver driver = new ChromeDriver();
+        String productName = "Sauce Labs Fleece Jacket";
+        
+        ChromeOptions options = new ChromeOptions();
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+        prefs.put("profile.password_manager_leak_detection", false);
+        options.setExperimentalOption("prefs", prefs);
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
+        
         Properties prop = new Properties();
         FileInputStream fis = new FileInputStream(System.getProperty("user.dir")
                 + "/sauce_labs_selenium/src/main/java/com/aditya/resources/GlobalData.properties");
         prop.load(fis);
-        String login_email = prop.getProperty("email");
+        String login_as_standard_user_email = prop.getProperty("std_user_email");
         String login_password = prop.getProperty("password");
-        driver.get("https://sauce-demo.myshopify.com/");
-        driver.findElement(By.id("customer_login_link")).click();
-        driver.findElement(By.id("customer_email")).sendKeys(login_email);
-        driver.findElement(By.id("customer_password")).sendKeys((login_password));
-        driver.findElement(By.xpath("//input[@value='Sign In']")).click();
-        driver.findElement(By.xpath("//ul/li/a[text()='Catalog']")).click();
-        // driver.findElement(By.xpath("//ul[@id='main-menu']/li/a[text()='Home']")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath(String.format(
-    "//h3[translate(normalize-space(.),'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')='%s']",
-    productName.toLowerCase()
-))).click();
-        // driver.findElement(By.id("add")).click();
-// WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
-// wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath(String.format(
-//         "//a[starts-with(translate(normalize-space(.), " +
-//         "'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), '%s')]",
-//         "Check Out".toLowerCase()
-//     )))));
-WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        
+        driver.get("https://www.saucedemo.com/");
 
-WebElement addToCartButton = driver.findElement(By.id("add"));
+        driver.findElement(By.id("user-name")).sendKeys(login_as_standard_user_email);
+        driver.findElement(By.id("password")).sendKeys((login_password));
+        driver.findElement(By.id("login-button")).click();
 
-// Initial state: enabled → click
-wait.until(ExpectedConditions.elementToBeClickable(addToCartButton)).click();
+        List<WebElement> products = driver.findElements(By.xpath("//div[@data-test='inventory-item']"));
+        WebElement productElement = products.stream()
+                .filter(product -> product.findElement(By.xpath(".//div[@data-test='inventory-item-name']"))
+                        .getText().equals(productName))
+                .findFirst().orElse(null);
+        productElement.findElement(By.xpath(".//button[text()='Add to cart']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions
+                .elementToBeClickable(driver.findElement(By.xpath("//a[@data-test='shopping-cart-link']"))));
+        driver.findElement(By.xpath("//a[@data-test='shopping-cart-link']")).click();
+        List<WebElement> cartProducts = driver.findElements(By.xpath("//div[@data-test='inventory-item-name']"));
 
-// Wait for the button to become disabled
-wait.until(d ->
-        !d.findElement(By.id("add")).isEnabled()
-);
-
-// Wait for the button to become enabled again
-wait.until(d ->
-        d.findElement(By.id("add")).isEnabled()
-);
-
-// Now the Add to Cart operation is complete
-// Go to Checkout
-driver.findElement(By.xpath(String.format(
-        "//a[starts-with(translate(normalize-space(.), " +
-        "'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), '%s')]",
-        "Check Out".toLowerCase()
-))).click();
-
-// Thread.sleep(4000);
-// driver.findElement(By.xpath(String.format(
-//         "//a[starts-with(translate(normalize-space(.), " +
-//         "'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), '%s')]",
-//         "Check Out".toLowerCase()
-//     ))).click();
-Boolean isProductInCartPage = driver.findElements(By.cssSelector(".description h3 a"))
-        .stream()
-        .anyMatch(element ->
-                element.getText().toLowerCase().contains(productName.toLowerCase())
-        );
-        Assert.assertTrue(isProductInCartPage);
-
-        driver.findElement(By.cssSelector("input[id='checkout']")).click();
-        // String cartPageProductText = driver.findElement(By.xpath("//div[@class='info']/h3/a")).getText();
-        // Assert.assertTrue(cartPageProductText.contains(productName));
-        // driver.findElement(By.xpath("//div[@class='actions']/input[@value='Check Out']")).click();
+        Boolean isExpectedItemInCart = cartProducts.stream()
+                .anyMatch(cartProduct -> cartProduct.getText().equals(productName));
+        Assert.assertTrue(isExpectedItemInCart);
+        driver.findElement(By.id("checkout")).click();
+        driver.findElement(By.id("first-name")).sendKeys("Aditya");
+        driver.findElement(By.id("last-name")).sendKeys("Pathak");
+        driver.findElement(By.id("postal-code")).sendKeys("474020");
+        driver.findElement(By.id("continue")).click();
+        driver.findElement(By.id("finish")).click();
+        String message = driver.findElement(By.xpath("//h2[data-test='complete-header']")).getText();
+        Assert.assertTrue(message.equalsIgnoreCase("Thank you for your order!"));
         driver.close();
     }
+
 }
